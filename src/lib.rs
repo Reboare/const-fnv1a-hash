56fa5fb7e91d056deb6d14515ee2af86270c9450
Booj <josiah.beverton@gmail.com>
2022-01-01 22:57:05 +0000

    Add Case Comparison and tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,11 @@ const FNV_OFFSET_BASIS_64: u64 = 0xcbf29ce484222325;
 const FNV_PRIME_32: u32 = 0x01000193;
 const FNV_PRIME_64: u64 = 0x00000100000001B3;
 
+const ASCII_CASE_MASK: u8 = 0b0010_0000;
+
 /// Computes 64-bits fnv1a hash of the given slice, or up-to limit if provided.
 /// If limit is zero or exceeds slice length, slice length is used instead.
-pub const fn fnv1a_hash_64(bytes: &[u8], limit: Option<usize>) -> u64 {
+pub const fn fnv1a_hash_64(bytes: &[u8], limit: Option<usize>, case: bool) -> u64 {
     let mut hash = FNV_OFFSET_BASIS_64;
 
     let mut i = 0;
@@ -24,7 +26,13 @@ pub const fn fnv1a_hash_64(bytes: &[u8], limit: Option<usize>) -> u64 {
     };
 
     while i < len {
-        hash ^= bytes[i] as u64;
+        let value = if case && (bytes[i] & ASCII_CASE_MASK == ASCII_CASE_MASK) {
+            bytes[i] ^ ASCII_CASE_MASK
+        }
+        else {
+            bytes[i]
+        };
+        hash ^= value as u64;
         hash = hash.wrapping_mul(FNV_PRIME_64);
         i += 1;
     }
@@ -33,7 +41,7 @@ pub const fn fnv1a_hash_64(bytes: &[u8], limit: Option<usize>) -> u64 {
 
 /// Computes 32-bits fnv1a hash of the given slice, or up-to limit if provided.
 /// If limit is zero or exceeds slice length, slice length is used instead.
-pub const fn fnv1a_hash_32(bytes: &[u8], limit: Option<usize>) -> u32 {
+pub const fn fnv1a_hash_32(bytes: &[u8], limit: Option<usize>, case: bool) -> u32 {
     let mut hash = FNV_OFFSET_BASIS_32;
 
     let mut i = 0;
@@ -49,7 +57,13 @@ pub const fn fnv1a_hash_32(bytes: &[u8], limit: Option<usize>) -> u32 {
     };
 
     while i < len {
-        hash ^= bytes[i] as u32;
+        let value = if case && (bytes[i] & ASCII_CASE_MASK == ASCII_CASE_MASK) {
+            bytes[i] ^ ASCII_CASE_MASK
+        }
+        else {
+            bytes[i]
+        };
+        hash ^= value as u32;
         hash = hash.wrapping_mul(FNV_PRIME_32);
         i += 1;
     }
@@ -62,7 +76,7 @@ pub const fn fnv1a_hash_32(bytes: &[u8], limit: Option<usize>) -> u32 {
 /// If limit is zero or exceeds slice length, slice length is used instead.
 #[inline(always)]
 pub const fn fnv1a_hash_16_xor(bytes: &[u8], limit: Option<usize>) -> u16 {
-    let bytes = fnv1a_hash_32(bytes, limit).to_ne_bytes();
+    let bytes = fnv1a_hash_32(bytes, limit, false).to_ne_bytes();
     let upper: u16 = u16::from_ne_bytes([bytes[0], bytes[1]]);
     let lower: u16 = u16::from_ne_bytes([bytes[2], bytes[3]]);
     upper ^ lower
@@ -71,17 +85,36 @@ pub const fn fnv1a_hash_16_xor(bytes: &[u8], limit: Option<usize>) -> u16 {
 /// Computes 64-bit fnv1a hash from a str.
 #[inline(always)]
 pub const fn fnv1a_hash_str_64(input: &str) -> u64 {
-    fnv1a_hash_64(input.as_bytes(), None)
+    fnv1a_hash_64(input.as_bytes(), None, false)
 }
 
 /// Computes 32-bit fnv1a hash from a str.
 #[inline(always)]
 pub const fn fnv1a_hash_str_32(input: &str) -> u32 {
-    fnv1a_hash_32(input.as_bytes(), None)
+    fnv1a_hash_32(input.as_bytes(), None, false)
 }
 
 /// Computes 16-bit fnv1a hash from a str using XOR folding.
 #[inline(always)]
 pub const fn fnv1a_hash_str_16_xor(input: &str) -> u16 {
     fnv1a_hash_16_xor(input.as_bytes(), None)
+}
+
+
+#[test]
+fn fnv1a_test_case_comparison() {
+    let bytes = ['A' as u8, 'B' as u8];
+    assert_eq!(fnv1a_hash_64(&bytes, None, false), fnv1a_hash_64(&bytes, None, true));
+    assert_eq!(fnv1a_hash_32(&bytes, None, false), fnv1a_hash_32(&bytes, None, true));
+
+    let bytes = ['a' as u8, 'B' as u8];
+    assert_ne!(fnv1a_hash_64(&bytes, None, false), fnv1a_hash_64(&bytes, None, true));
+    assert_ne!(fnv1a_hash_32(&bytes, None, false), fnv1a_hash_32(&bytes, None, true));
+
+    let bytes = ['a' as u8, 'B' as u8];
+    let comparison = ['A' as u8, 'B' as u8];
+    assert_eq!(fnv1a_hash_64(&bytes, None, true), fnv1a_hash_64(&comparison, None, true));
+    assert_eq!(fnv1a_hash_32(&bytes, None, true), fnv1a_hash_32(&comparison, None, true));
+
+
 }
